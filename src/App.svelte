f8d370c2853b2aca3f880a5841fc2bd78a03b56f
Domin <teesdomin@gmail.com>
2020-02-25 21:39:30 +0100

    App fix

--- src/App.svelte
+++ src/App.svelte
@@ -1,92 +1,78 @@
 <script>
-	import ToDo from './ToDo.svelte';
-	import { fade } from 'svelte/transition';
-	let src = "./Vector 1.svg";
+  import ToDo from "./ToDo.svelte";
+  let src = "./Vector 1.svg";
 
-	let tasks = JSON.parse(localStorage.getItem("tasks"));
-	console.log(tasks);
+  let uid = 1;
 
-	const UpdateLocalStorage = () => {
-		localStorage.setItem("tasks", JSON.stringify(tasks));
-		tasks = JSON.parse(localStorage.getItem("tasks"));
-		console.log(`local storage tasks array has been updated. Presenting the array:`);
-		console.table(tasks);
-	}
+  let tasks = [
+    {
+	  uid: uid++,
+	  complete: false,
+      description: "test"
+    },
+    {
+	  uid: uid++,
+	  complete: false,
+      description: "test 2"
+    }
+  ];
 
-	const CheckCurrentLocalStorage = () => {
-		console.log(`current local storage array is:`);
-		console.table(tasks);
-	}
-	
-	const AddTask = () => {
-		tasks.push(document.getElementById("add-task").value);
-		console.log(`Task has been added`);
-		UpdateLocalStorage();
-		document.getElementById("add-task").value = "";
-	}
+  function addTask (input) {
+    const task = {
+	  uid: uid++,
+	  complete: false,
+      description: input.value
+    };
+    tasks = [task, ...tasks];
+    input.value = "";
+  };
 
-	const deleteTask = (x) => {
-		tasks.splice(tasks.indexOf(x),1);
-		UpdateLocalStorage();
-	}
-	
-	const HandleKey = (event) => {
-		if(event.key=="Enter"){
-			AddTask();
-		}
-	}
+  function removeTask(event) {
+	const { uid } = event.detail;
+	tasks = tasks.filter(e => e.uid !== uid);
+  }
 
+  function updateStatus(event) {
+	const { uid, newStatus } = event.detail;
+	tasks = tasks.map(e => e.uid === uid ? {...e, complete: newStatus} : e);
+  }
 </script>
 
-<svelte:window on:keydown={HandleKey}/>
-
 <main>
-	<h1>todo</h1>
-	<input id="add-task"><button on:click={()=>AddTask()} >add</button>
-	<button on:click={()=>CheckCurrentLocalStorage()}>check local storage</button>
-	{#each tasks as x}
-	<div class="eventListener todo-wrap" on:click={()=>deleteTask(x)}>
-	<ToDo task={x}></ToDo>
-	</div>
-	{/each}
+  <h1>todo</h1>
+  <input
+    placeholder="what needs to be done?"
+    on:keydown={e => e.which === 13 && addTask(e.target)} />
+
+  {#each tasks as task (task.uid)}
+      <ToDo {...task} on:remove={removeTask} on:toggle={updateStatus}></ToDo>
+  {/each}
 </main>
 
 <style>
-@import url('https://rsms.me/inter/inter.css');
+  @import url("https://rsms.me/inter/inter.css");
 
-	*{
-		font-family: 'Inter';
+  * {
+    font-family: "Inter";
+  }
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	}
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	.todo-wrap:hover{
-		cursor: pointer;
-		font-family: "Inter";
-		
-		/*background: #ff3e00;*/
-	}
-	.todo-wrap{
-		width:30%;
-		margin: 0 auto;
-		font-size: 16px;
-	}
-	
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
--- src/ToDo.svelte
+++ src/ToDo.svelte
@@ -1,10 +1,51 @@
 <script>
-    export let task;
     import { fade } from 'svelte/transition';
+    import { createEventDispatcher } from 'svelte';
 
-    
+    const dispatch = createEventDispatcher();
+
+    function remove() {
+        dispatch('remove', {uid})
+    }
+
+	function toggleStatus() {
+        let newStatus = !complete;
+		dispatch('toggle', {
+            uid,
+            newStatus
+        });
+    }
+
+    export let uid;
+    export let description;
+    export let complete;
 </script>
 
-<div class="task-container">
-    <h4 transition:fade="{{delay: 250, duration: 300}}">{task}</h4>
+<div class="todo-item" transition:fade="{{delay: 250, duration: 300}}">
+    {#if complete}
+    <span class="is-complete">{ description }</span>
+    <button on:click={toggleStatus}> ✔️ </button>
+    {:else}
+        <span>{ description }</span>
+        <button on:click={toggleStatus}> ❌ </button>
+    {/if}
+    <button on:click={remove}> 🗑 </button>
 </div>
+
+<style>
+.todo-item {
+    position: relative;
+    line-height: 1.2;
+    padding: 0.5em 2.5em 0.5em 2em;
+    margin: 0 0 0.5em 0;
+    border-radius: 2px;
+    user-select: none;
+    border: 1px solid hsl(240, 8%, 70%);
+    background-color: hsl(240, 8%, 93%);
+    color: #333;
+  }
+.is-complete {
+    text-decoration: line-through;
+    color: green;
+}
+</style>
